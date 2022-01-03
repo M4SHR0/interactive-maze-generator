f097bf74a3a0d945ec757c911d16fc1f4ba30898
mashro <fall.est.mori@gmail.com>
2022-01-03 22:48:45 +0900

    ブロックのbool値を取得するメソッドの作成

--- MazeGenerator/Tile/Tile.java
+++ MazeGenerator/Tile/Tile.java
@@ -4,5 +4,7 @@ abstract public class Tile{
   protected int rotate;
   protected boolean[][][] block;
   
-  
+  boolean block(int rotate,int row,int column){
+    return block[rotate][row][column];
+  }
 }
